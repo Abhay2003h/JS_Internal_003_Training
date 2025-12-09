4f4713361b172c8646404ebc4ba85df76fa6702e
Abhay2003h <abhaykumar27373@gmail.com>
2025-12-09 05:27:46 +0000

    second

---
+++ basic/day_2.js
@@ -0,0 +1,49 @@
+/*
+Display All the value of above var.
+let a=23
+const b="abc"
+var c=78
+d=88
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+Try to change val of each and every variable and display error if any.
+let a=56
+const b="abcd"
+var c=788
+d=888
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+Redeclear var wwith same name and intitalize with new value and display thr op.
+let a=56
+const b="abcd"
+var c=788
+d=888
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+*/
+
+/*
+let a="abc"
+let b=""
+let c=null
+let d;
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+console.log(typeof(null))*/
+
+let score="22abc"
+console.log(typeof(score))
+console.log(typeof score)
+let score2=Number(score)
+console.log(score2)
